039466ec38a64dfe341ca49e9ea4fa701c36ad7e
sandercrommelinck <sandcrom2@student.arteveldehs.be>
2020-02-28 17:17:20 +0100

    Update data.js
    Link

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/f2051bf9-7f35-4d9f-75ad-10dce6b00330-8434/",
                 "target": "_blank"
             }
         ]
